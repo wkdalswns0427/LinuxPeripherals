794684065430239533f5db7caf5dbb5ac9004855
MinJun <68832065+wkdalswns0427@users.noreply.github.com>
2023-03-07 11:25:04 +0900

    Update main.py

--- python/main.py
+++ python/main.py
@@ -8,19 +8,16 @@ import datetime as dt
 from src.commands import Commands
 import src.aes128 as aes
 from src.customcrc16 import CRC16_CCITTFALSE
+import src.keyfile as keyfile
+from src.utils import utils
 
-SERVER_IP = 'serverip'
+encrypt_key = keyfile.str_encrypt_key
+IV = keyfile.str_IV
+
+SERVER_IP = '192.168.11.20'
 SERVER_PORT = 12242
-SIZE = 128
+SIZE = 512
 SERVER_ADDR = (SERVER_IP, SERVER_PORT)
-
-# def socketServerSetup():
-#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)     
-#     server_socket.bind((ip_addr, port))
-#     server_socket.listen(1)
-#     return server_socket
-
 
 def socketClientSetup():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,48 +26,71 @@ def socketClientSetup():
     return client_socket
 
 
-
 def socketRead(client_socket):
     crcagent = CRC16_CCITTFALSE()
-    aesagent = aes.AES128Crypto()
+    aesagent = aes.AES128Crypto(encrypt_key, IV)
     commands = Commands()
-    # client, addr = server_socket.accept()
+    util = utils()
+    # client, addr = client_socket.accept()
 
     while True:
         content = client_socket.recv(SIZE)
-        print("data : ", content)
+        print("---------------------------------------------------------")
+        print("read data : ", content, "type : ", type(content))
+        print("read data list : ", list(content),  "type : ", type(list(content)[1]))
 
         if len(content) == 0:
             break
         
         # respond to kiosk access request 0x10
         elif content[3] == 0x10 and crcagent.crcVerifyXMODEM(content):
-            commands.sendCORERESP(client_socket, content)
+            commands.sendCORERESP(client_socket=client_socket, content=content)
 
-        # check 0x20 command ACK
-        elif content[3] == 0x20 and crcagent.crcVerifyXMODEM(content):
-            SEQ = [content[1], content[2]] # seq data according to datasheet
-            ENCRYPTED_DATA = content[6:21]
-            DECRYPTED_DATA = aesagent.decrypt(ENCRYPTED_DATA, SEQ)
+        # check 0xE0 command ACK
+        elif content[3] == 0xE0 and crcagent.crcVerifyXMODEM(content):
+            # need to return ACK in 30ms
+            ret = commands.sendACK(client_socket, content)
+            print("SEND ACK : ", ret)
+
+            SEQ = [content[1], content[2]]
+            DATA = list(content[6:22]) 
+
+            print("* * * * * * * * * * * * * * * * * * * * * * * *")
+            decrypted_data = aesagent.decrypt(DATA, SEQ)
+            info = decrypted_data[0:8]
+            issue_info = decrypted_data[8:]
+            info = util.list2str(list(info))
+            issue_info = util.list2str(list(issue_info))
+            print("dec_data : ",info)
+            print("dec_data_issue_ : ",issue_info)
+            print("* * * * * * * * * * * * * * * * * * * * * * * *")
+            
+            ret = util.postdata(info, issue_info)
+            print("POST : ", ret)
+
+        # check 0xE0 command NACK
+        elif content[3] == 0xE0 and not crcagent.crcVerifyXMODEM(content):
+            print("SEND NACK")
+            commands.sendNACK(client_socket, content)
+        
+        elif content[3] == 0xE2 and crcagent.crcVerifyXMODEM(content):
             ########################################
-            ####     DO SOMETHING WITH DATA     ####
+            #####  React to INFO DEL REQUESTS  #####
             ########################################
             commands.sendACK(client_socket, content)
-
-        # check 0x20 command NACK
-        elif content[3] == 0x20 and not crcagent.crcVerifyXMODEM(content):
-            commands.sendNACK(client_socket, content)
-
+            pass
+            
         else:
-            print(content) 
+            SyntaxWarning("Invalid request") 
+            pass
 
     print("Closing connection")
     client_socket.close()
 
-    
 def main():
     client_socket = socketClientSetup()
     socketRead(client_socket)
+    
 
 if __name__=="__main__":
     main()
