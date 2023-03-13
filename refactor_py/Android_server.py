dd3d16a23eaf444c1f250bbef8dc6d154d340627
MinJun <68832065+wkdalswns0427@users.noreply.github.com>
2023-03-13 11:40:56 +0900

    Update Android_server.py

--- refactor_py/Android_server.py
+++ refactor_py/Android_server.py
@@ -20,29 +20,40 @@ class YpassSocketServer():
         self.android_socket, self.android_addr = self.server_socket.accept()
     
     def makeData(self):
-        data = self.util.readFcsv("./data.csv")
+        data = self.util.readFcsv("./obu_data.csv")
         data = self.util.str2hexstr(data[1] + data[2])
+        output = ''
         
         header = [0x02, 0x10, 0xA0]
         tail = [0xff,0xff, 0x03]
         crc_data = bytes( header + data + tail )
         crc_h, crc_l = self.crcagent.makeCRC(crc_data)
-        tail[-3] = crc_h; tail[-2] = crc_l
-        
-        full_data = header + data + tail
-        
-        for d in full_data:
-            output = output + str(d) + "!"
-        
-        return bytes(output, 'utf-8')
+
+        if crc_h != self.previousCRC[0] and crc_l != self.previousCRC[1]:
+            self.previousCRC[0], self.previousCRC[1] = crc_h, crc_l
+            tail[-3] = crc_h; tail[-2] = crc_l
+            
+            full_data = header + data + tail
+
+            for d in full_data:
+                output = output + str(d) + "!"
+            
+            return bytes(output, 'utf-8')
+        else:
+            return False
     
     def send2Android(self):
         
         senddata = self.makeData()
-        self.android_socket.sendall(bytes(senddata, encoding='utf-8'))
+        if senddata != False:
+            self.android_socket.sendall(senddata)
 
 
 def main():
     socketServer = YpassSocketServer()
     socketServer.serverConnect()
-    socketServer.send2Android()
+    while(True):
+        socketServer.send2Android()
+
+if __name__ == "__main__":
+    main()
